31a1724e1bb71bd3efbd5c022be2b451ad83085f
sarthak0699 <sarthak0699@gmail.com>
2024-03-06 20:38:56 -0700

    Added changes

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import csv
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
+from fastapi import BackgroundTasks
+import boto3
 
 app = FastAPI()
 origins = ["*"]
+request_queue_url = "https://sqs.us-east-1.amazonaws.com/339712806862/1225316534-req-queue"
+
+sqs = boto3.client('sqs',region_name = 'us-east-1')
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,11 +29,23 @@ async def load_classification_results():
         for row in reader:
             image_results[row[0]] = row[1]
 
+async def autoscaling_controller():
+    ec2_resources = boto3.resource('ec2')
+    
+    while True:
+        instances = ec2_resources.instances
+        count = len(list(instances))
+
+        response = sqs.send_message(QueueUrl=request_queue_url,MessageBody=str(count))
+        print(response)
+        # response = ec2_client.describe_instances()
+        
+        
 
 # Load classification results asynchronously during startup
 @app.on_event("startup")
-async def startup_event():
-    await load_classification_results()
+async def startup_event(background_tasks:BackgroundTasks):
+    return 
 
 
 @app.get("/check", tags=["Root"])
